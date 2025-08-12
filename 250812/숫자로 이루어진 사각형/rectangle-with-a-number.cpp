219eb6c3b20232db349b4add5eee11926d969f50
MountainHS <67748022+MountainHS@users.noreply.github.com>
2025-08-12 21:32:29 +0900

    Title: 숫자로 이루어진 사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250812/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
+++ 250812/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -8,6 +8,7 @@ void PrintNN(int n){
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             cout << (count % 9)+1 << " ";
+            count++;
         }
         cout << "\n";
     }
